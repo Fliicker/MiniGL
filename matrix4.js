32e2a089c017746321337860b623168f93be5b98
fliicker <723833066@qq.com>
2024-12-04 22:24:47 +0800

    20241204

--- matrix4.js
+++ matrix4.js
@@ -1,4 +1,8 @@
 let m4 = {
+  identity: function () {
+    return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+  },
+
   translation: function (tx, ty, tz) {
     return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, tx, ty, tz, 1]
   },
