80e897944c3e8ac2875abd151cf8cf6ae7e34cbb
fliicker <723833066@qq.com>
2024-12-09 20:23:22 +0800

    mvp-matrix

--- matrix3.js
+++ matrix3.js
@@ -1,37 +1,21 @@
 let m3 = {
   projection: function projection(width, height) {
     // Note: This matrix flips the Y axis so that 0 is at the top.
-    return [
-      2 / width, 0, 0,
-      0, -2 / height, 0,
-      -1, 1, 1,
-    ];
+    return [2 / width, 0, 0, 0, -2 / height, 0, -1, 1, 1];
   },
 
   translation: function translation(tx, ty) {
-    return [
-      1, 0, 0,
-      0, 1, 0,
-      tx, ty, 1,
-    ];
+    return [1, 0, 0, 0, 1, 0, tx, ty, 1];
   },
 
   rotation: function rotation(angleInRadians) {
     var c = Math.cos(angleInRadians);
     var s = Math.sin(angleInRadians);
-    return [
-      c, -s, 0,
-      s, c, 0,
-      0, 0, 1,
-    ];
+    return [c, -s, 0, s, c, 0, 0, 0, 1];
   },
 
   scaling: function scaling(sx, sy) {
-    return [
-      sx, 0, 0,
-      0, sy, 0,
-      0, 0, 1,
-    ];
+    return [sx, 0, 0, 0, sy, 0, 0, 0, 1];
   },
 
   multiply: function multiply(a, b) {
@@ -66,16 +50,34 @@ let m3 = {
     ];
   },
 
-  translate: function(m, tx, ty) {
+  translate: function (m, tx, ty) {
     return m3.multiply(m, m3.translation(tx, ty));
   },
 
-  rotate: function(m, angleInRadians) {
+  rotate: function (m, angleInRadians) {
     return m3.multiply(m, m3.rotation(angleInRadians));
   },
 
-  scale: function(m, sx, sy) {
+  scale: function (m, sx, sy) {
     return m3.multiply(m, m3.scaling(sx, sy));
+  },
+
+  cross: function (a, b) {
+    return [a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0]];
+  },
+
+  subtractVectors: function (a, b) {
+    return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+  },
+
+  normalize: function (v) {
+    var length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+    // 确定不会除以 0
+    if (length > 0.00001) {
+      return [v[0] / length, v[1] / length, v[2] / length];
+    } else {
+      return [0, 0, 0];
+    }
   },
 };
 
