7b0d055fab972fca3cfa503131cff8a139a34bae
fliicker <723833066@qq.com>
2024-12-11 14:46:17 +0800

    primary-light

--- matrix4.js
+++ matrix4.js
@@ -246,6 +246,15 @@ let m4 = {
     ];
   },
 
+  transpose: function(m) {
+    return [
+      m[0], m[4], m[8], m[12],
+      m[1], m[5], m[9], m[13],
+      m[2], m[6], m[10], m[14],
+      m[3], m[7], m[11], m[15],
+    ];
+  },
+  
   // 左下角为原点 --> 左上角为原点
   projection: function (width, height, depth) {
     // 注意：这个矩阵翻转了 Y 轴，所以 0 在上方
