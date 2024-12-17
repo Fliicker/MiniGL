8ffa261ce3b2dd7a14e00032200b3725a69fcc1a
ld <723833066@qq.com>
2024-12-17 11:35:42 +0800

    draw-geojson-line

--- src/controlCenter.js
+++ src/controlCenter.js
@@ -2,9 +2,8 @@ import { bearing } from "@turf/turf";
 import m4 from "./matrix4";
 
 export default class ControlCenter {
-
-  isDraggingA = false;  // 左键拖动
-  isDraggingB = false;  // 中键拖动
+  isDraggingA = false; // 左键拖动
+  isDraggingB = false; // 中键拖动
   clientHeight = 0;
   clientHeight = 0;
 
@@ -14,24 +13,24 @@ export default class ControlCenter {
   };
 
   cameraState = {
-    position: [0, 0, 0],    //  TODO: 由地图中心和zoom解算
+    position: [0, 0, 0], //  TODO: 由地图中心和zoom解算
     up: [0, 1, 0],
     target: [0, 0, 0],
-    direction: [0, 0, 1]
+    direction: [0, 0, 1],
   };
 
   viewState = {
-    pitch: 60,
+    pitch: 90,
     bearing: 0,
-    z: 0,
-    center: [0, 0]
-  }
+    z: 50,
+    center: [0, 0],
+  };
 
   perspectiveState = {
     fov: this.degToRad(60),
     near: 10,
-    far: 200,
-  }
+    far: 500,
+  };
 
   modelMatrix = m4.identity();
   viewMatrix = m4.identity();
@@ -52,9 +51,12 @@ export default class ControlCenter {
 
   updateCameraMatrix() {
     this.calcCameraState();
-    console.log(this.cameraState)
     // let cameraMatrix = m4.lookAtByDirection(this.cameraState.position, this.cameraState.direction, this.cameraState.up);
-    let cameraMatrix = m4.lookAt(this.cameraState.position, this.cameraState.target, this.cameraState.up);
+    let cameraMatrix = m4.lookAt(
+      this.cameraState.position,
+      this.cameraState.target,
+      this.cameraState.up
+    );
     this.viewMatrix = m4.inverse(cameraMatrix);
   }
 
@@ -77,21 +79,48 @@ export default class ControlCenter {
   calcCameraState() {
     let bearing = this.degToRad(this.viewState.bearing);
     let pitch = this.degToRad(this.viewState.pitch);
-    let dx = this.viewState.z * Math.sin(bearing) * Math.cos(bearing) / Math.sin(-pitch);
-    let dy = this.viewState.z / Math.tan(pitch);
-
-    this.cameraState.position = [this.viewState.center[0] + dx, this.viewState.center[1] + dy, this.viewState.z];
+    let dx, dy;
+    if (this.viewState.pitch === 90) {
+      (dx = 0), (dy = 0);
+    } else {
+      dx = (this.viewState.z * Math.sin(bearing)) / Math.tan(pitch);
+      dy = -(this.viewState.z * Math.cos(bearing)) / Math.tan(pitch);
+    }
+    // console.log("dx:", dx, "  dy:", dy);
+    // console.log("bearing:", this.viewState.bearing);
+    // console.log("pitch:", this.viewState.pitch);
+    this.cameraState.position = [
+      this.viewState.center[0] + dx,
+      this.viewState.center[1] + dy,
+      this.viewState.z,
+    ];
     this.cameraState.target = [this.viewState.center[0], this.viewState.center[1], 0];
+    this.cameraState.up = [-Math.sin(bearing), Math.cos(bearing), 0];
+    let r = Math.sqrt(
+      Math.pow(this.cameraState.position[0] - this.cameraState.target[0], 2) +
+        Math.pow(this.cameraState.position[1] - this.cameraState.target[1], 2) +
+        Math.pow(this.cameraState.position[2] - this.cameraState.target[2], 2)
+    );
   }
 
   get mvpMatrix() {
     let vpMatrix = m4.multiply(this.projectionMatrix, this.viewMatrix);
     let mvpMat = m4.multiply(vpMatrix, this.modelMatrix);
-    return mvpMat
+    return mvpMat;
+  }
+
+  get modelITMatrix() {
+    let modelInverseMatrix = m4.inverse(this.modelMatrix);
+    let modelITMatrix = m4.transpose(modelInverseMatrix);
+    return modelITMatrix;
   }
 
   degToRad(d) {
     return (d * Math.PI) / 180;
+  }
+
+  clamp(number, min, max) {
+    return Math.max(min, Math.min(max, number));
   }
 
   handleKeyDown(e) {
@@ -129,23 +158,36 @@ export default class ControlCenter {
         let dx = e.movementX / this.clientWidth;
         let dy = -e.movementY / this.clientHeight;
         let inverseProjectionMatrix = m4.inverse(this.projectionMatrix);
-        let transposeViewMatrix = m4.transpose(this.viewMatrix)
-        let viewRotationMatrix = m4.removeTranslation(transposeViewMatrix)
-        let inverseMatrix = m4.multiply(viewRotationMatrix, inverseProjectionMatrix)
-        let translateOnView = m4.multiplyMatrixVector(inverseMatrix, [dx, dy, 0, 0.15])   // TODO: 如何实现精确解算?
-        this.cameraState.position[0] += translateOnView[0] / translateOnView[2];
-        this.cameraState.position[1] += translateOnView[1] / translateOnView[2];
-        console.log(translateOnView);
-        cb()
+        let transposeViewMatrix = m4.transpose(this.viewMatrix);
+        let viewRotationMatrix = m4.removeTranslation(transposeViewMatrix);
+        let inverseMatrix = m4.multiply(viewRotationMatrix, inverseProjectionMatrix);
+        let translateOnView = m4.multiplyMatrixVector(inverseMatrix, [dx, dy, 0, 0.15]); // TODO: 如何实现精确解算?
+        // this.cameraState.center[0] += translateOnView[0] / translateOnView[2];
+        // this.cameraState.center[1] += translateOnView[1] / translateOnView[2];
+        this.viewState.center[0] += translateOnView[0] / translateOnView[2];
+        this.viewState.center[1] += translateOnView[1] / translateOnView[2];
+        console.log(translateOnView[0] / translateOnView[2]);
+        cb();
       }
       if (this.isDraggingB) {
         let dx = e.movementX;
         let dy = -e.movementY;
-        this.viewState.bearing -= this.degToRad(5 * dx)
-        this.viewState.pitch += this.degToRad(5 * dy)
-        this.calcCameraState()
-        cb()
+        let pitch1 = this.degToRad(this.viewState.pitch);
+        let z1 = this.viewState.z;
+        this.viewState.bearing = (this.viewState.bearing - 0.3 * dx) % 360;
+        this.viewState.pitch = this.clamp(this.viewState.pitch - 0.3 * dy, 30, 90);
+        let pitch2 = this.degToRad(this.viewState.pitch);
+        this.viewState.z = (z1 * Math.sin(pitch2)) / Math.sin(pitch1);
+        this.calcCameraState();
+        cb();
       }
+    });
+
+    window.addEventListener("wheel", (e) => {
+      let d = e.deltaY / 10;
+      this.viewState.z = this.clamp(this.viewState.z + d, 10, 500);
+      this.calcCameraState();
+      cb();
     });
   }
 }
--- src/geojsonRenderer.js
+++ src/geojsonRenderer.js
@@ -11,19 +11,20 @@ const gl = canvas.getContext("webgl2");
 
 const modelData = {
   vertices: null,
-  pointCount: 0,
-}
+  lines: null,
+};
 
 let program;
 let controlCenter = new ControlCenter(gl.canvas.clientWidth, gl.canvas.clientHeight);
 
 const main = async () => {
   const geojson = (await axios.get("/geojson/China.json")).data;
+  // var geojson = turf.simplify(geojsonRaw, { tolerance: 100, highQuality: false });
   const center = turf.center(geojson).geometry.coordinates;
   const bbox = turf.bbox(geojson);
-  console.log(bbox)
+  console.log(bbox);
   controlCenter.cameraState.position = [center[0], center[1], 70];
-  controlCenter.viewState.z = 70;
+  controlCenter.viewState.z = 100;
   controlCenter.viewState.center = center;
 
   let vs = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
@@ -33,21 +34,20 @@ const main = async () => {
   let vao = gl.createVertexArray();
   gl.bindVertexArray(vao);
 
-  let positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  modelData.vertices = getVertices(geojson)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(modelData.vertices), gl.STATIC_DRAW);
-  modelData.pointCount = modelData.vertices.length;
+  // let positionBuffer = gl.createBuffer();
+  // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  modelData.vertices = getVertices(geojson);
+  modelData.lines = getLines(geojson);
+  // let allData = modelData.vertices.concat(modelData.lines.flat());
+  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(allData), gl.STATIC_DRAW);
 
-  gl.enableVertexAttribArray(0);
-  gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+  // gl.enableVertexAttribArray(0);
+  // gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
 
   drawScene();
 
-  controlCenter.initEvents(drawScene)
+  controlCenter.initEvents(drawScene);
 };
-
-window.addEventListener("keydown", handleKeyDown, true);
 
 function drawScene() {
   resizeCanvasToDisplaySize(gl.canvas);
@@ -60,86 +60,95 @@ function drawScene() {
 
   controlCenter.updateAllMatrix();
   let matrixLocation = gl.getUniformLocation(program, "u_mvpMatrix");
+  let modelMatrixLocation = gl.getUniformLocation(program, "u_model");
+  let modelITLocation = gl.getUniformLocation(program, "u_modelInverseTranspose");
+  let lightPosLocation = gl.getUniformLocation(program, "u_lightWorldPosition");
+  let viewPosLocation = gl.getUniformLocation(program, "u_viewWorldPosition");
+  let uDrawModeLocation = gl.getUniformLocation(program, "u_drawMode");
+
   gl.uniformMatrix4fv(matrixLocation, false, controlCenter.mvpMatrix);
-  gl.drawArrays(gl.TRIANGLES, 0, modelData.pointCount)
-}
+  gl.uniformMatrix4fv(modelMatrixLocation, false, controlCenter.modelMatrix);
+  gl.uniformMatrix4fv(modelITLocation, false, controlCenter.modelITMatrix);
+  gl.uniform3fv(lightPosLocation, [120, 70, 250]);
+  gl.uniform3fv(viewPosLocation, controlCenter.cameraState.position);
 
-function handleKeyDown(e) {
-  if (e.ctrlKey) {
-    if (e.key === "ArrowUp") {
-      modelStates.rotation[0] -= degToRad(1);
-    }
+  let triangleBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(modelData.vertices), gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(0);
+  gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
 
-    if (e.key === "ArrowDown") {
-      modelStates.rotation[0] += degToRad(1);
-    }
+  gl.uniform1i(uDrawModeLocation, 0);
+  gl.drawArrays(gl.TRIANGLES, 0, modelData.vertices.length / 3);
 
-    if (e.key === "ArrowRight") {
-      modelStates.rotation[1] += degToRad(1);
-    }
+  // let lineBuffer = gl.createBuffer();
+  // gl.bindBuffer(gl.ARRAY_BUFFER, lineBuffer);
+  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(modelData.lines[0]), gl.STATIC_DRAW);
+  // gl.enableVertexAttribArray(0);
+  // gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+  // gl.uniform1i(uDrawModeLocation, 1);
+  // gl.lineWidth(10.0);
+  // gl.drawArrays(gl.LINE_STRIP, 0, modelData.lines[0].length / 3);
 
-    if (e.key === "ArrowLeft") {
-      modelStates.rotation[1] -= degToRad(1);
-    }
+  let lineBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, lineBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(modelData.lines.flat()), gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(0);
+  gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+  gl.uniform1i(uDrawModeLocation, 1);
+  gl.lineWidth(10.0);
 
-    if (e.key === "q") {
-      modelStates.rotation[2] += degToRad(1);
-    }
+  // modelData.lines.forEach((line) => {
+  //   let lineBuffer = gl.createBuffer();
+  //   gl.bindBuffer(gl.ARRAY_BUFFER, lineBuffer);
+  //   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(modelData.lines), gl.STATIC_DRAW);
+  //   gl.drawArrays(gl.LINE_STRIP, 0, line.length / 3);
+  // })
 
-    if (e.key === "e") {
-      modelStates.rotation[2] -= degToRad(1);
-    }
-  } else if (e.shiftKey) {
-    if (e.key === "ArrowLeft") {
-      cameraStates.position[0] -= 10;
-    }
-
-    if (e.key === "ArrowRight") {
-      cameraStates.position[0] += 10;
-    }
-  } else {
-    if (e.key === "ArrowLeft") {
-      modelStates.translation[0] -= 0.05;
-    }
-
-    if (e.key === "ArrowRight") {
-      modelStates.translation[0] += 0.05;
-    }
-
-    if (e.key === "ArrowUp") {
-      modelStates.translation[1] += 0.05;
-    }
-
-    if (e.key === "ArrowDown") {
-      modelStates.translation[1] -= 0.05;
-    }
-  }
-
-  drawScene();
+  let offset = 0;
+  modelData.lines.forEach((line) => {
+    gl.drawArrays(gl.LINE_STRIP, offset, line.length / 3); // 绘制每条线
+    offset += line.length / 3; // 更新偏移量
+  });
 }
 
 function getVertices(geojson) {
   let vertices = [];
   turf.featureEach(geojson, (polygon) => {
     const array = polygonToArray(polygon);
-    vertices.push(...array)
+    vertices.push(...array);
   });
 
   return vertices;
+}
+
+function getLines(geojson) {
+  let lineArray = []
+  turf.featureEach(geojson, (polygon) => {
+    const lines = turf.polygonToLine(polygon);
+    turf.flattenEach(lines, (line) => {
+      let lineData = line.geometry.coordinates.map((coord) => [...coord, 0.01]).flat();
+      lineArray.push(lineData)
+    });
+  });
+  return lineArray
 }
 
 function polygonToArray(polygon) {
   let array = [];
   let triangles = turf.tesselate(polygon);
   turf.flattenEach(triangles, (triangle) => {
-    let triangleData = triangle.geometry.coordinates[0].slice(0, 3).map(coord => [...coord, 0]).flat();
+    let triangleData = triangle.geometry.coordinates[0]
+      .slice(0, 3)
+      .map((coord) => [...coord, 0])
+      .flat();
     array.push(...triangleData);
   });
-  
+
   return array;
 }
 
-function convertToMercator() {}
+function convertToMercator() { }
 
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
@@ -186,7 +195,5 @@ function resizeCanvasToDisplaySize(canvas) {
 
   return needResize;
 }
-
-
 
 main();
